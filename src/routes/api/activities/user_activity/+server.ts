acb2d4d3c1b374a0106acad77d7483ef661bd7c0
Wiktor Pytlewski <01149938@pw.edu.pl>
2024-06-15 21:15:48 +0200

    modify routes

---
+++ src/routes/api/activities/user_activity/+server.ts
@@ -0,0 +1,13 @@
+import { sql } from '$lib/server/db';
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async ({ url }) => {
+	// const [{ message }] = await sql`SELECT 'Hello world!' as message`;
+    let creator_id = url.searchParams.get('user_id')
+    const activities = await sql`SELECT a.id, a.name as activity_name, a.status, a.performer_id
+                                FROM activities a
+                                        LEFT OUTER JOIN users u
+                                                        ON u.id = a.performer_id
+                                        WHERE a.creator_id=${creator_id}`;
+	return json({activities});
+};
