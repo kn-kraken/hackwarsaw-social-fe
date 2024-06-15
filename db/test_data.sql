74c6c307689b056f67314164bd1d2a6c4c733a09
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-06-15 18:14:30 +0200

    updated schema

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,11 +1,7 @@
 
 INSERT INTO activities VALUES (1, 10, 'act1');
-INSERT INTO regions VALUES (1, 'Wola');
-INSERT INTO regions VALUES (2, 'Praga');
 INSERT INTO users VALUES (1, 'IA');
 INSERT INTO users_in_activities VALUES (1, 1);
 
 SELECT pg_catalog.setval('public.activities_id_seq', 1, true);
-SELECT pg_catalog.setval('public.posts_id_seq', 1, false);
-SELECT pg_catalog.setval('public.regions_id_seq', 2, true);
 SELECT pg_catalog.setval('public.users_id_seq', 1, true);
