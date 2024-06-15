52e1c030b6f68a3f2ca1ffca1abc329ebf928bee
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-06-15 18:44:53 +0200

    status

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO users (name) VALUES ('Jan Kowalski');
 INSERT INTO users (name) VALUES ('Stefan Baczyński');
-INSERT INTO activities (points, name, creator_id) VALUES (10, 'Usuwanie graffiti', 1);
-INSERT INTO activities (points, name, creator_id) VALUES (15, 'Sprzątanie śmieci', 2);
+INSERT INTO activities (points, name, creator_id, status) VALUES (10, 'Usuwanie graffiti', 1, 'CREATED');
+INSERT INTO activities (points, name, creator_id, status) VALUES (15, 'Sprzątanie śmieci', 2, 'CREATED');
