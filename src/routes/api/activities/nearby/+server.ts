7b3dce14b229975a3303e71f352a56fdd03091db
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-06-15 22:49:09 +0200

    fix comment

--- src/routes/api/activities/nearby/+server.ts
+++ src/routes/api/activities/nearby/+server.ts
@@ -2,7 +2,7 @@ import { sql } from '$lib/server/db';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async ({ url }) => {
-    // enpoint na aktywnosci stworzone przez uzytkownika
+    // endpoint for nearby activites
     // TODO: pass actual location as an argument and filter accordingly
     const activities = await sql`SELECT a.id, a.name as activity_name, a.status, a.performer_id, a.location
                                 FROM activities a`;
