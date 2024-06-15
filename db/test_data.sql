6350fe3ab06b252ac66308153e8b406d79f45eec
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2024-06-15 18:43:22 +0200

    new schema

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,7 +1,5 @@
 
-INSERT INTO activities VALUES (1, 10, 'act1');
-INSERT INTO users VALUES (1, 'IA');
-INSERT INTO users_in_activities VALUES (1, 1);
-
-SELECT pg_catalog.setval('public.activities_id_seq', 1, true);
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+INSERT INTO users (name) VALUES ('Jan Kowalski');
+INSERT INTO users (name) VALUES ('Stefan Baczyński');
+INSERT INTO activities (points, name, creator_id) VALUES (10, 'Usuwanie graffiti', 1);
+INSERT INTO activities (points, name, creator_id) VALUES (15, 'Sprzątanie śmieci', 2);
